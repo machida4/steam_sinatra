f397415790591777b69cf6e8b05f70cf10db7dfe
machida <machida@kmc.gr.jp>
2019-07-05 05:07:33 +0000

    ActiveRecordを有効化

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
-require 'sinatra'
-require 'sinatra/reloader'
-
 require 'rubygems'
 require 'bundler'
 
 Bundler.require
+
+set :database, {adapter: "sqlite3", database: "games.sqlite3"}
+
+class Game < ActiveRecord::Base
+  validates_presence_of :name
+end
 
 get '/' do
   "Hello, world!"
